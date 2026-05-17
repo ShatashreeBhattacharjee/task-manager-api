99ad351212439a418d8e06ba120f14ee5c1af353
Shatashree Bhattacharjee <shatashreebhattacharjee25@gmail.com>
2026-05-17 14:40:19 +0530

    Switch to PostgreSQL

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
+from datetime import timedelta
 class Config:
-    SQLALCHEMY_DATABASE_URI='sqlite:///tasks.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:052005%40Sb@localhost:5432/taskmanager'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     JWT_SECRET_KEY='Your-secret-key-change-this'
+    JWT_ACCESS_TOKEN_EXPIRES=timedelta(hours=1)
+
 
