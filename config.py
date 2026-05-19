9761a8bdf55276f3d401cef4b6ef1a2677511de6
Shatashree Bhattacharjee <shatashreebhattacharjee25@gmail.com>
2026-05-19 20:38:03 +0530

    Add Docker configuration

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 from datetime import timedelta
+import os
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:052005%40Sb@localhost:5432/taskmanager'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
+                                             'postgresql://postgres:newpassword123@localhost:5432/taskmanager')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     JWT_SECRET_KEY='Your-secret-key-change-this'
     JWT_ACCESS_TOKEN_EXPIRES=timedelta(hours=1)
